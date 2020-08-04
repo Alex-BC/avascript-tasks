0b553516251d48c5b241f1f8caa3aa6508bd84be
Alexandr Chepak <djalexbc@gmail.com>
2020-08-04 16:26:13 +0300

    Update task-2-1

--- js/task-2-1.js
+++ js/task-2-1.js
@@ -7,7 +7,13 @@
 // Используйте вспомогательную переменную result для добавления(конкатенации) строк внутри цикла for
 
 const getItemsString = function (array) {
+  'use strict';
   // Write code under this line
+  let result = '';
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+  return result;
 };
 
 console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
@@ -28,3 +34,12 @@ console.log(getItemsString([5, 10, 15]));
 3 - 15
 '
 */
+
+// Этапы
+// 1. Код должен содержать функцию getItemsString
+// Результатом вызова функции должна быть строка, если array массив
+// Результатом вызова функции должна быть пустая строка, если array - пустой массив
+// Для массива строк результат не совпадает с ожидаемым
+// Для массива чисел результат не совпадает с ожидаемым
+// Ожидается использование метода 'for'
+// Ожидается использование метода 'length'
