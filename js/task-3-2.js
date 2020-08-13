0cbff5cb7fc4801a6e3049ad506ed7e9cf944f80
Alexandr Chepak <djalexbc@gmail.com>
2020-08-13 20:32:54 +0300

    Update task-3-3

--- js/task-3-2.js
+++ js/task-3-2.js
@@ -7,6 +7,9 @@
 const countProps = function (obj) {
   'use strict';
   // Write code under this line
+
+  const keys = Object.keys(obj);
+  return keys.length;
 };
 
 console.log(countProps({})); // 0
@@ -14,3 +17,11 @@ console.log(countProps({})); // 0
 console.log(countProps({ a: 1, b: 1 })); // 2
 
 console.log(countProps({ a: 1, b: 1, c: 1, d: 1, e: 1 })); // 5
+
+// Этапы
+// Ожидается что countProps является функцией
+// Ожидается что функция вернет 0 для пустого объекта
+// Ожидается что функция вернет 2 для объекта { a: 1, b: 1 }
+// Ожидается что функция вернет 5 для объекта { a: 1, b: 1, c: 1, d: 1, e: 1 }
+// Ожидается использование метода 'Object.keys'
+// Ожидается использование свойства 'length'
