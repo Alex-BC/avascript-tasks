023864c958d2acc988de6d24c378f4c5f30ee357
Alexandr Chepak <djalexbc@gmail.com>
2020-08-27 16:56:35 +0300

    Update task-5-3

--- js/task-5-1.js
+++ js/task-5-1.js
@@ -5,10 +5,13 @@
 // В prototype функции - конструктора добавь метод getInfo(), который возвращает строку со значениями свойств login и email объекта.
 
 // Write code under this line
-const Account = function ({ login, email } = {}) {};
+const Account = function (login, email) {
+  this.login = login;
+  this.email = email;
+};
 
 Account.prototype.getInfo = function (login, email) {
-  return `login : ${login}, email: ${email}`;
+  return `login : ${this.login}, email: ${this.email}`;
 };
 
 console.log(typeof Account.prototype.getInfo);
@@ -21,3 +24,11 @@ console.log(mango.getInfo());
 const poly = new Account('Poly', 'poly@mail.com');
 console.log(poly.getInfo());
 // 'login : Poly, email: poly@mail.com'
+
+// Код должен содержать функцию - конструктор 'Account'
+// Функция - конструктор 'Account' должна создавать объект
+// Значение Account.prototype.getInfo должно быть функцией
+// Объект, созданный вызовом new Account('Mangozedog', 'mango@dog.woof') должен содержать свойство 'login' со значением 'Mangozedog' и свойство 'email' со значением 'mango@dog.woof'
+// Объект, созданный вызовом new Account('Mangozedog', 'mango@dog.woof') при вызове метода getInfo() должен возвращать строку 'login : Mangozedog, email: mango@dog.woof'
+// Объект, созданный вызовом new Account('Poly', 'poly@mail.com') должен содержать свойство 'login' со значением 'Poly' и свойство 'email' со значением 'poly@mail.com'
+// Объект, созданный вызовом new Account('Poly', 'poly@mail.com') при вызове метода getInfo() должен возвращать строку 'login : Poly, email: poly@mail.com'
